3da69ee177f58f5be7ee2f0f2e3f4e0b981fcfc3
Anil <anilsahu9786@gmail.com>
2024-08-04 23:03:01 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/B-planet-Summer-Internship/',
+  base: '/B-planet-Summer-Internship/',
   plugins: [react()],
 })
